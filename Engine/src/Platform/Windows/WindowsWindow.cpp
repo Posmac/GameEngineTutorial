62bcd8aba41fb9a1548cd7d3e0355af160f34368
Posmac <nicolai.posmac@gmail.com>
2022-07-25 15:48:44 +0300

    Added glad to a project

--- Engine/src/Platform/Windows/WindowsWindow.cpp
+++ Engine/src/Platform/Windows/WindowsWindow.cpp
@@ -5,6 +5,8 @@
 #include "RefraEngine/Events/MouseEvent.h"
 #include "RefraEngine/Events/KeyEvent.h"
 
+#include <glad/glad.h>
+
 namespace rfe
 {
 	static bool s_GLFWInitialized = false;
@@ -48,6 +50,7 @@ namespace rfe
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(),
 			nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
 
--- premake5.lua
+++ premake5.lua
@@ -13,8 +13,10 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 --Include directories relative to root folder (solution dir)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Engine/vendor/GLFW/include"
+IncludeDir["Glad"] = "Engine/vendor/Glad/include"
 
 include "Engine/vendor/GLFW"
+include "Engine/vendor/Glad"
 --END
 
 project "Engine"
@@ -38,12 +40,14 @@ project "Engine"
 	{
 		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}"
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}"
 	}
 	
 	links
 	{
 		"GLFW",
+		"Glad",
 		"opengl32.lib"
 	}
 	
@@ -55,7 +59,8 @@ project "Engine"
 		defines
 		{
 			"RF_PLATFORM_WINDOWS",
-			"RF_BUILD_DLL"
+			"RF_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
 		}
 		
 		postbuildcommands
